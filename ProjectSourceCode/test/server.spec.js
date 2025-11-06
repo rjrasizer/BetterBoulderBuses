26d590fed0bda78cccb897234aa995c97512b72e
Reed <resh2365@colorado.edu>
2025-11-06 13:13:37 -0700

    Added skeleton of tests

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -31,3 +31,57 @@ describe('Server!', () => {
 
 // ********************************************************************************
 
+describe('Testing Add User API', () => {
+  it('positive : /add_user', done => {
+    chai
+      .request(server)
+      .post('/add_user')
+      .send({id: 5, name: 'John Doe', dob: '2020-02-20'})
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equals('Success');
+        done();
+      });
+  });
+});
+
+it('Negative : /add_user. Checking invalid name', done => {
+    chai
+      .request(server)
+      .post('/add_user')
+      .send({id: '5', name: 10, dob: '2020-02-20'})
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body.message).to.equals('Invalid input');
+        done();
+      });
+  });
+});
+
+describe('Testing Redirect', () => {
+  // Sample test case given to test /test endpoint.
+  it('\test route should redirect to /login with 302 HTTP status code', done => {
+    chai
+      .request(server)
+      .get('/test')
+      .end((err, res) => {
+        res.should.have.status(302); // Expecting a redirect status code
+        res.should.redirectTo(/^.*127\.0\.0\.1.*\/login$/); // Expecting a redirect to /login with the mentioned Regex
+        done();
+      });
+  });
+});
+
+describe('Testing Render', () => {
+  // Sample test case given to test /test endpoint.
+  it('test "/login" route should render with an html response', done => {
+    chai
+      .request(server)
+      .get('/login') // for reference, see lab 8's login route (/login) which renders home.hbs
+      .end((err, res) => {
+        res.should.have.status(200); // Expecting a success status code
+        res.should.be.html; // Expecting a HTML response
+        done();
+      });
+  });
+});
